7265b15ead5b44a4c190e5c1a7d3b4db4e028b7c
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-09 16:34:47 +0530

    feat: added retriever

---
+++ src/retrieval/retriever.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+def load_user_vectorstore(user_id: str) -> Chroma:
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    persist_dir = Path(f"db/chroma/{user_id}")
+    if not persist_dir.exists():
+        raise FileNotFoundError(f"User {user_id} has no vectorstore.")
+
+    vectorstore = Chroma(
+        collection_name=f"{user_id}_pdf_collection",
+        persist_directory=str(persist_dir),
+        embedding_function=embedding_model
+    )
+
+    return vectorstore
+
+def get_retriever(
+    vectorstore: Chroma,
+    search_type: str = "mmr",
+    k: int = 4
+):
+    return vectorstore.as_retriever(
+        search_type=search_type,
+        search_kwargs={"k": k}
+    )
