409c93f97f6068f2436b8d302b6afe6994f3e17a
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-09 16:05:04 +0530

    feat: updated test for embedding

--- tests/embedding_test.py
+++ tests/embedding_test.py
@@ -3,9 +3,10 @@ from src.embeddings.build_vectorstore import build_user_vectorstore
 def run_test():
     vs = build_user_vectorstore("user_001")
 
-    results = vs.similarity_search("What are the subjects?", k=3)
+    results = vs.similarity_search("What is machine learning?", k=3)
     
     for result in results:
+        print("----------------------------")
         print(result.page_content)
         print(result.metadata)
 
